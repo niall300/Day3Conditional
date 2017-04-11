0ac36ee07332bc708667b40d1de26a12155bb0ac
niall coffey <niall300@yahoo.co.uk>
2017-04-11 15:00:26 +0100

    day 3

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day_3
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        int N = Convert.ToInt32(Console.ReadLine());
+
+
+        if (N % 2 > 0)
         {
+            Console.WriteLine("Weird");
+
+        }
+        else
+        {
+            //If N is even and in the inclusive range of 2  to 5, print Not Weird
+            if (N >= 2 && N <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            //If N is even and in the inclusive range of 6 to 20, print Weird
+            else if (N >= 6 && N <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            //If N is even and greater than 20, print Not Weird
+            else
+            {
+                Console.WriteLine("Not Weird");
+
+            }
+
         }
     }
 }
